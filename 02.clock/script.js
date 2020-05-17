405cf3c411deb493987efa7045f2ec315042d36f
Md. Rakibul Hasan <md.rakibulhasan@Mds-iMac.local>
2020-05-18 00:43:00 +0600

    clock project added

---
+++ 02.clock/script.js
@@ -0,0 +1,21 @@
+const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".minute-hand");
+const hourHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegree = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegree}deg)`;
+
+  const minutes = now.getMinutes();
+  const minuteDegree = (minutes / 60) * 360 + (seconds / 60) * 6 + 90;
+  minuteHand.style.transform = `rotate(${minuteDegree}deg)`;
+
+  const hours = now.getHours();
+  const hourDegree = (hours / 12) * 360 + (minutes / 60) * 30 + 90;
+  hourHand.style.transform = `rotate(${hourDegree}deg)`;
+}
+
+setInterval(setDate, 1000);
